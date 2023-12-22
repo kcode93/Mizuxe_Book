065975b9fbbe57758cf21bcba18b3439fe4affce
Kenneth Garcia <kennethgarcia12@hotmail.com>
2023-12-22 13:57:10 -0800

    if applied, this commit adds the footer markup/styles, and the scroll-spy & smooth scroll features

--- dist/resources/js/main.js
+++ dist/resources/js/main.js
@@ -1,1 +1,19 @@
-//hello world
+//get current Year
+$('#year').text(new Date().getFullYear());
+
+
+//init scroll spy
+$('body').scrollspy({target: '#main-navbar'});
+
+//smooth Scrolling
+$('#main-navbar a').on('click', function(event){
+    if(this.hash !== ""){
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+    }
+});
